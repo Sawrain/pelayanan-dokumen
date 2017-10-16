3fc57d2e8570eab101dee4c1d67cdc3eded649a8
Endy Muhardin <endy.muhardin@gmail.com>
2017-10-16 13:57:57 +0700

    starter project

---
+++ src/main/java/id/ac/tazkia/akademik/dokumen/pelayanandokumen/PelayananDokumenApplication.java
@@ -0,0 +1,12 @@
+package id.ac.tazkia.akademik.dokumen.pelayanandokumen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelayananDokumenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PelayananDokumenApplication.class, args);
+	}
+}
