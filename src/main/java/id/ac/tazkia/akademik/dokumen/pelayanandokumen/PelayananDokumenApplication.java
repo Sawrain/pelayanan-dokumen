adc7e0e56c4fcba74477b9d20367a8e953c3012e
Endy Muhardin <endy.muhardin@gmail.com>
2017-10-16 14:09:51 +0700

    display index page

--- src/main/java/id/ac/tazkia/akademik/dokumen/pelayanandokumen/PelayananDokumenApplication.java
+++ src/main/java/id/ac/tazkia/akademik/dokumen/pelayanandokumen/PelayananDokumenApplication.java
@@ -2,11 +2,21 @@ package id.ac.tazkia.akademik.dokumen.pelayanandokumen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class PelayananDokumenApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PelayananDokumenApplication.class, args);
+	}
+
+	@Controller
+	class IndexController {
+		@GetMapping("/")
+		public String index(){
+			return "index";
+		}
 	}
 }
