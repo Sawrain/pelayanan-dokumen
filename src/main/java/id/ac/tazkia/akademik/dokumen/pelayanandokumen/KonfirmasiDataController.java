471adca94454911ece550b740e37429b96164443
Sawrain <ramadhani@tazkia.ac.id>
2017-10-17 15:52:57 +0700

    Layout Front end

---
+++ src/main/java/id/ac/tazkia/akademik/dokumen/pelayanandokumen/KonfirmasiDataController.java
@@ -0,0 +1,14 @@
+package id.ac.tazkia.akademik.dokumen.pelayanandokumen;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class KonfirmasiDataController {
+
+    @GetMapping("/konfirmasi")
+    public void konfirmasiData(){
+
+    }
+
+}
